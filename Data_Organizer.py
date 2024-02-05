30c2239a445854a3b90eaf61c27a3d8baada50b1
Gabriel <gabbm9233@gmail.com>
2024-02-05 19:41:57 -0300

    teste 1 deu certo, e enviar para o diretório 1 também

--- Data_Organizer.py
+++ Data_Organizer.py
@@ -45,23 +45,23 @@ option = input("Selecione o número correspondente ao tipo de arquivo: ");
 match option:
     case "1":
         output_path = f"{output_directory}/Data_filtered.xlsx"
-        new_dataframe.to_excel('Data_filtered.xlsx', index=False);      
+        new_dataframe.to_excel(output_path, index=False);      
         print("Dados salvos em 'Data_filtered.xlsx'"); 
     case "2":
         output_path = f"{output_directory}/Data_filtered.csv"
-        new_dataframe.to_csv('Data_filtered.csv', index=False);      
+        new_dataframe.to_csv(output_path, index=False);      
         print("Dados salvos em 'Data_filtered.csv'"); 
     case "3":
         output_path = f"{output_directory}/Data_filtered.parquet"
-        new_dataframe.to_parquet('Data_filtered.parquet', index=False);      
+        new_dataframe.to_parquet(output_path, index=False);      
         print("Dados salvos em 'Data_filtered.parquet'"); 
     case "4":
         output_path = f"{output_directory}/Data_filtered.h5"
-        new_dataframe.to_hdf('Data_filtered.h5', index=False);      
+        new_dataframe.to_hdf(output_path, index=False);      
         print("Dados salvos em 'Data_filtered.h5'"); 
     case "5":
         output_path = f"{output_directory}/Data_filtered.json"
-        new_dataframe.to_json('Data_filtered.json', index=False);      
+        new_dataframe.to_json(output_path, index=False);      
         print("Dados salvos em 'Data_filtered.json'"); 
     case _:
         print("Por favor, selecione uma opção válida!"); 
